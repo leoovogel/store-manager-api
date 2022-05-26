de6e253d10b1ccd887e50e73ddbee18a003b813a
Leonardo Vogel <leeo.vog13@gmail.com>
2022-05-26 03:39:54 -0300

    feat: add error message if sale not exists

--- controllers/sales.controller.js
+++ controllers/sales.controller.js
@@ -3,21 +3,26 @@ const rescue = require('express-rescue');
 const { StatusCodes } = require('http-status-codes');
 
 const salesService = require('../services/sales.service');
+const { SALE_NOT_FOUND } = require('../utils/errorMessages');
 
 const router = express.Router();
 
-router.get('/:id', rescue(async (req, res) => {
+router.get('/:id', rescue(async (req, res, next) => {
   const { id } = req.params;
 
   const [sales] = await salesService.getSaleById(id);
 
-  res.status(StatusCodes.OK).json(sales);
+  if (!sales.length) {
+    return next(SALE_NOT_FOUND);
+  }
+
+  return res.status(StatusCodes.OK).json(sales);
 }));
 
 router.get('/', rescue(async (_req, res) => {
   const [sales] = await salesService.getSales();
 
-  res.status(StatusCodes.OK).json(sales);
+  return res.status(StatusCodes.OK).json(sales);
 }));
 
 module.exports = router;
--- utils/errorMessages.js
+++ utils/errorMessages.js
@@ -1,3 +1,4 @@
 module.exports = {
-    PRODUCT_NOT_FOUND: { status: 404, message: 'Product not found' },
+  PRODUCT_NOT_FOUND: { status: 404, message: 'Product not found' },
+  SALE_NOT_FOUND: { status: 404, message: 'Sale not found' },
 };
