f406d8012800b20b1e795da8f24a78cdc47837ef
Leonardo Vogel <leeo.vog13@gmail.com>
2022-05-26 02:46:47 -0300

    feat: add error message to product not found

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,22 +1,28 @@
 const express = require('express');
+const rescue = require('express-rescue');
 const { StatusCodes } = require('http-status-codes');
 
 const productsService = require('../services/products.service');
+const { PRODUCT_NOT_FOUND } = require('../utils/errorMessages');
 
 const router = express.Router();
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', rescue(async (req, res, next) => {
   const { id } = req.params;
 
   const [product] = await productsService.getProductById(id);
 
-  res.status(StatusCodes.OK).json(product[0]);
-});
+  if (!product.length) {
+    return next(PRODUCT_NOT_FOUND);
+  }
 
-router.get('/', async (_req, res) => {
+  return res.status(StatusCodes.OK).json(product[0]);
+}));
+
+router.get('/', rescue(async (_req, res) => {
   const [products] = await productsService.getProducts();
 
   res.status(StatusCodes.OK).json(products);
-});
+}));
 
 module.exports = router;
---
+++ utils/errorMessages.js
@@ -0,0 +1,3 @@
+module.exports = {
+    PRODUCT_NOT_FOUND: { status: 404, message: 'Product not found' },
+};
