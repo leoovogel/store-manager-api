6b446b72a25b89d7659c3122f3f14929f1199880
Leonardo Vogel <leeo.vog13@gmail.com>
2022-05-30 15:10:23 -0300

    build: add package-lock and changes on connect.js

--- models/connect.js
+++ models/connect.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
   database: 'StoreManager',
 });
 
